ea37eae3da44527ab96988b0108214636b8d7665
Loa212 <anobileloris@gmail.com>
2022-10-04 11:49:49 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Pages/**/*.cshtml",
+    "./Pages/**/*.razor",
+    "./Views/**/*.chstml",
+    "./Views/**/*.razor",
+    "./Shared/**/*.razor",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
